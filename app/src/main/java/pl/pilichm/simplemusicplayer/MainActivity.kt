fa915b663fd5c4bfcfd8092fc028abd6eb6c7169
michalpilichowski <michal.pilichowski@onet.pl>
2023-09-03 19:18:51 +0200

    Init repo - missing files.

---
+++ app/src/main/java/pl/pilichm/simplemusicplayer/MainActivity.kt
@@ -0,0 +1,10 @@
+package pl.pilichm.simplemusicplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
